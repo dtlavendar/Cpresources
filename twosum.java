5f0b9209bc6b510998b6986ca6955a54d470bb37
dtlavendar <69658493+dtlavendar@users.noreply.github.com>
2020-12-04 22:42:50 -0500

    Create twosum.java
    um twosum but in java

---
+++ twosum.java
@@ -0,0 +1,105 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class main {
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        }
+        public int[] nextArray(int n) {
+            int a[] = new int[n];
+            for(int i =0; i < n; i++) a[i]=this.nextInt();
+            return a;
+        }
+        public void randomShuffleSort(int []a) {
+            for(int i =0; i < (a.length)/2; i++) {
+                int temp=a[i];
+                a[i]=a[(a.length)-1-i];
+                a[(a.length)-1-i]=temp;
+            }
+            this.fine_sort(a);
+        }
+        public void fine_sort(int a[]) {
+            ArrayList<Integer> A = new ArrayList<>();
+            for(int i : a) A.add(i);
+            Collections.sort(A);
+            for(int i=0; i < a.length; i++) {
+                a[i]=A.get(i);
+            }
+        } 
+    }
+    public static void main(String args[]) {
+        // Twosum equation
+        FastReader fr = new FastReader();
+        int size = fr.nextInt();
+        int target = fr.nextInt();
+        HashMap<Integer, Integer> arr = new HashMap<>();
+        int elem[] = fr.nextArray(size);
+        boolean works=true;
+        fr.randomShuffleSort(elem);
+        for(int i =0; i < size; i++) {
+            if(arr.containsKey(elem[i])) {
+                System.out.println(arr.get(elem[i]), i+1);
+                works=false;
+            } else {
+                arr.put(target-elem[i], i+1);
+            }
+        }
+        if(works) {
+            System.out.println("Nothing even works anymore lmfao")
+        }
+
+    } 
+}
