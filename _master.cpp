4f624c440d2d9826ad5431043fbd7b46b6773d65
dtlavendar <69658493+dtlavendar@users.noreply.github.com>
2020-12-26 18:00:13 -0500

    Update _master.cpp

--- _master.cpp
+++ _master.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define atoi(x) (x-'0')
 #define cnst const
 using ll = long long;
-
+#define endl '\n'
 #define ar array
 #define it int64_t
 #define vt vector
@@ -17,23 +17,38 @@ const ll inf = ll(1e18);
 #define FORn(i, n) for (int i = 0; i < int(n); i++)
 #define offset(V, i)  for (auto &v : V) v = i
 #define all(x) x.begin(), x.end()
-#define f first
-#define s second
+const int mod = 1e9;
 #define minel(x) (*min_element(all(x)))
+void dbg_out() {cerr << endl;}
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__);
+
 struct edge {
     ll from;
     ll to;
     ll cost;
+};
+struct DSU {
+	vector<int> e; void init(int N) { e = vector<int>(N,-1); }
+	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); } 
+	bool sameSet(int a, int b) { return get(a) == get(b); }
+	int size(int x) { return -e[get(x)]; }
+	bool unite(int x, int y) { // union by size
+		x = get(x), y = get(y); if (x == y) return 0;
+		if (e[x] > e[y]) swap(x,y);
+		e[x] += e[y]; e[y] = x; return 1;
+	}
 };
 bool operator<(edge t, edge f) {
     return t.cost > f.cost;
 }
 struct fast_mod
 {
-
-    int mod = 1e9 + 7;
     template <typename T>
-
     bool isEven(T x)
     {
         return !(x & 1);
@@ -134,9 +149,13 @@ struct segtree
 {
 
     struct segtree *lChild;
+    
     struct segtree *rChild;
+    
     ll sum;
+    
     int leftmost, rightmost;
+    
     segtree(int left, int right, vector<int> &arr)
     {
 
@@ -150,12 +169,14 @@ struct segtree
         {
             int mid = (left + right) / 2;
 
+    
             lChild = new segtree(left, mid, arr);
             rChild = new segtree(mid + 1, right, arr);
             this->update();
         }
     }
     void update(){
+    
         if(leftmost==rightmost) return;
         sum=lChild->sum+rChild->sum;
     }
@@ -164,6 +185,7 @@ struct segtree
             sum=newVal;
             return;
         }
+    
         if(index<=lChild->rightmost) lChild->pointUpdate(index, newVal);
         else rChild->pointUpdate(index, newVal);
         update();
@@ -183,26 +205,36 @@ struct segtree
 };
 struct udGraph {
     vector<vector<int>> adjList;
+    
     vector<bool> vis;
+    
     int size;
     udGraph(int n) {
         this->size = n;
+        
         vis.resize(n);
+        
         adjList.resize(n);
     }
     void addNodes(int u, int v) {
+        
         adjList[u].push_back(v);
+        
         adjList[v].push_back(u);
     }
     void dfs(int n) {
         vis[n]=true;
+
         for(auto &edge: adjList[n]) if(!this->vis[edge]) dfs(edge);
     }
     bool connected() {
         this->dfs(0);
+
         int n = this->size;
+
         for(int i =0; i < n; i++)
             if(!this->vis[i]) return false;
+        
         return true;
         // O(n)
     }
@@ -228,34 +260,61 @@ bool binsearch(vector<T> &nums, int target)
 }
 namespace number_theory {
 	vector<int> sieve(int n) {
-		bool prime[15000105];
-        for(int i =0; i < n; i++) prime[i]=1;
+		bool prime[150000];
+
+        for(int i =0; i <= n; i++) prime[i]=1;
+
 		for(int i = 2; i <= n; i++)if(prime[i]) for(int j=i*i; j <= n; j+=i) prime[j]=0;
+
 		vector<int> ans;
+
 		for(int i =2; i <=n; i++) if(prime[i]) ans.push_back(i);
+
 		return ans;
 	}
     vector<ll> pfactors(int n) {
         vector<ll> pf;
         for(ll i = 2; i < n/2; i++) {
+
             int c=0;
+
             while(n%i==0) {
                 c++;
                 n/=i;
             }
+
             if(c) pf.push_back(i);
         }
+
         return pf;
         // O(sqrt(n))
     }
+    vector<ll> factors(int n) {
+        set<ll> ans;
+        vector<ll> v;
+        for(int i =1; i * i < n; ++i) {
+            if(n%i==0) {
+                ans.insert(n/i);
+                ans.insert(i);
+            }
+        }
+
+        for(auto &factor: ans)
+            v.push_back(factor);
+        return v;
+    }
     bool isPrime(int n) {
         if(n == 1) return false;
+
         if(n == 2) return true;
+
         if(n == 3) return true;
+
         if(n%3 ==0 || n%2==0) return false;
-        for(int i = 5; i * i <=n; i+=6) {
+
+        for(int i = 5; i * i <=n; i+=6)
             if(n % i ==0 || n % (i+2) ==0) return false;
-        }
+
         return true;
     }
 
@@ -263,14 +322,13 @@ namespace number_theory {
 
 void solve()
 {
-	int n;
-	cin >> n;
+    
 }
 int main()
 {
     fastio;
 
-    int tc;
+    int tc = 1;
     cin >> tc;
     while(tc--){
         solve();
